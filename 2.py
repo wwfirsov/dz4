5c9a7db11993b3b3b9500b2833407efbb7e742c9
wwfirsov <65654471+wwfirsov@users.noreply.github.com>
2021-01-18 14:54:03 +0300

    Revert "Add files via upload"

--- 2.py
+++
@@ -1,6 +0,0 @@
-list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-print(f"Исходный список: {list}")
-new_list = []
-new_list = [el for el in list if el < list(el+1)] # как обратиться к следующему значению списка?
-
-print(f"Новый список: {new_list}")
